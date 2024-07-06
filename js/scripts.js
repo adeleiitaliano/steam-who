4886f34d2df173e6b0c11d40212435eda2ae031d
adeleiitaliano <adyita19@outlook.it>
2024-07-06 10:06:52 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+const carousel = document.querySelector(".carousel");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+let scrollAmount = 0;
+
+nextBtn.addEventListener("click", () => {
+  const cardWidth = document.querySelector(".card").offsetWidth + 56; 
+  const visibleCards = 3; 
+  if (scrollAmount < carousel.scrollWidth - cardWidth * visibleCards) {
+    scrollAmount += cardWidth;
+    carousel.style.transform = `translateX(-${scrollAmount}px)`;
+  }
+});
+
+prevBtn.addEventListener("click", () => {
+  const cardWidth = document.querySelector(".card").offsetWidth + 56; 
+  if (scrollAmount > 0) {
+    scrollAmount -= cardWidth;
+    carousel.style.transform = `translateX(-${scrollAmount}px)`;
+  }
+});
