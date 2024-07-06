d172257733db1339dfa9cc2b81e06bdd6257a757
adeleiitaliano <adyita19@outlook.it>
2024-07-06 19:07:56 +0200

    some adjustments

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,8 @@ const nextBtn = document.getElementById("nextBtn");
 let scrollAmount = 0;
 
 nextBtn.addEventListener("click", () => {
-  const cardWidth = document.querySelector(".card").offsetWidth + 56; 
-  const visibleCards = 3; 
+  const cardWidth = document.querySelector(".card").offsetWidth + 56;
+  const visibleCards = 3;
   if (scrollAmount < carousel.scrollWidth - cardWidth * visibleCards) {
     scrollAmount += cardWidth;
     carousel.style.transform = `translateX(-${scrollAmount}px)`;
@@ -14,9 +14,25 @@ nextBtn.addEventListener("click", () => {
 });
 
 prevBtn.addEventListener("click", () => {
-  const cardWidth = document.querySelector(".card").offsetWidth + 56; 
+  const cardWidth = document.querySelector(".card").offsetWidth + 56;
   if (scrollAmount > 0) {
     scrollAmount -= cardWidth;
     carousel.style.transform = `translateX(-${scrollAmount}px)`;
   }
+});
+
+document.querySelectorAll(".card").forEach((card) => {
+  card.addEventListener("click", () => {
+    card.querySelector(".card-inner").classList.toggle("is-flipped");
+  });
+});
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// Aggiungi un event listener al pulsante
+scrollToTopBtn.addEventListener("click", () => {
+  // Scorri la pagina verso l'alto
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth", // Scroll liscio
+  });
 });
